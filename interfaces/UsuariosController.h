b6aff4dc0689aed361a4481420cb4a6e8a1830f9
Santiago Alpuy <alpuyfing@gmail.com>
2016-06-01 16:23:29 -0400

    Cambio CR por windows

--- interfaces/UsuariosController.h
+++ interfaces/UsuariosController.h
@@ -1,17 +1,1 @@
-#ifndef LAB6_USUARIOSCONTROLLER_H
-#define LAB6_USUARIOSCONTROLLER_H
-
-#include <string>
-
-using namespace std;
-
-class UsuariosController: public IUsuarios {
-
-public:
-
-    void IngresarInmobiliaria(String nombre, String mail, String direccion) = 0;
-    void ReportesInmobiliaria() = 0;
-    void CheckEmail(mail: String) = 0;
-    void DarAltaInteresado(String nombre, String apellido, int email) = 0;
-};
-#endif //LAB6_USUARIOSCONTROLLER_H
+#ifndef LAB6_USUARIOSCONTROLLER_H#define LAB6_USUARIOSCONTROLLER_H#include <string>using namespace std;class UsuariosController: public IUsuarios {public:    void IngresarInmobiliaria(String nombre, String mail, String direccion) = 0;    void ReportesInmobiliaria() = 0;    void CheckEmail(mail: String) = 0;    void DarAltaInteresado(String nombre, String apellido, int email) = 0;};#endif //LAB6_USUARIOSCONTROLLER_H
